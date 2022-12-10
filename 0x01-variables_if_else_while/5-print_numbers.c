54db07f56f8e18626684843205da5b6880054980
Francis James <ibolokefrancisjames2@gmail.com>
2022-12-10 10:46:50 -0800

    base ten

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int nz;
+
+	for (nz = 0; nz <= 9; nz++)
+		printf("%d", nz);
+	printf("\n");
+	return (0);
+}
