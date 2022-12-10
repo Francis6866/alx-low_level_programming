9c18920f5715c889ef1509916d5f033776611b31
Francis James <ibolokefrancisjames2@gmail.com>
2022-12-10 06:06:42 -0800

    lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Lowercase letters
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lcl;
+
+	for (lcl = 'a'; lcl = <= 'z'; lcl++)
+	{
+		putchar(lcl);
+	}
+	putchar("\n");
+	return (0);
+}
