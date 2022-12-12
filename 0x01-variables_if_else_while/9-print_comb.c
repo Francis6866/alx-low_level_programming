793e456de8936fc7badaa14b02e80f808ef0a832
Francis James <ibolokefrancisjames2@gmail.com>
2022-12-12 14:11:40 -0800

    update

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 18; d < 28; d++)
+	{
+		putchar(d);
+		for (d != 27)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	putchar('\n');
+	}
+	return (0);
+}
