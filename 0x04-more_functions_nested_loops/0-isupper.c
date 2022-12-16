400f4eda2c70ef028c0e6980f7cfbdb57ffd06b7
Francis James <ibolokefrancisjames2@gmail.com>
2022-12-16 12:51:15 -0800

    upercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - Check the code
+ * @c: Input character
+ * Return: Return 1 if true and 0 if false
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
